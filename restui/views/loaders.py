4c7c8166b0e7397b0bb2afb6546f05b5451200c2
Alessandro Vullo <avullo@ebi.ac.uk>
2018-06-05 14:46:06 +0100

    Underlying serialisation relies on having data values in list format, so explicitly check this using assertions. Also, do not need the GET method at the moment, unless we want to leave it as a placeholder.

--- restui/views/loaders.py
+++ restui/views/loaders.py
@@ -2,8 +2,11 @@ from restui.models.ensembl import EnsemblGene
 from restui.serializers.ensembl import EnsemblGeneSerializer
 
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework.response import Response
+
 import pprint
 
 class EnsemblFeature(mixins.CreateModelMixin,
@@ -12,19 +15,23 @@ class EnsemblFeature(mixins.CreateModelMixin,
     serializer_class = EnsemblGeneSerializer
 
     def get_serializer(self, *args, **kwargs):
-        if "data" in kwargs:
-            data = kwargs["data"]
+        # method should have been passed request.data as 'data' keyword argument
+        assert "data" in kwargs, (
+            "data not present"
+        )
+        # data should come as a list of feature-like items
+        assert isinstance(kwargs["data"], list), (
+            "data is not a list"
+        )
 
-        if isinstance(data, list):
-            kwargs["many"] = True
+        # when a serializer is instantiated and many=True is passed,
+        # a ListSerializer instance will be created. The serializer
+        # class then becomes a child of the parent ListSerializer
+        kwargs["many"] = True
 
-            return super(EnsemblFeature, self).get_serializer(*args, **kwargs)
+        return super(EnsemblFeature, self).get_serializer(*args, **kwargs)
     
     def post(self, request, *args, **kwargs):
+        # this calls self.perform_create(self.get_serializer)
         objs = self.create(request, *args, **kwargs)
-        pprint.pprint(objs)
         return objs
-    
-    def get(self, request, *args, **kwargs):
-        print("Got here")
-        
