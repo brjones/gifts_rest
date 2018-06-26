33f29cba201ea446093b5deb19745c50059fadae
Alessandro Vullo <avullo@ebi.ac.uk>
2018-06-26 16:43:49 +0100

    Serialiser to create comments associated to mapping.

--- restui/serializers/annotations.py
+++ restui/serializers/annotations.py
@@ -1,4 +1,4 @@
-from restui.models.annotations import UeMappingStatus
+from restui.models.annotations import UeMappingStatus, UeMappingComment
 
 from rest_framework import serializers
 
@@ -16,4 +16,35 @@ class StatusSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = UeMappingStatus
+        exclude = ('user_stamp',)
+
+#
+# TODO
+#
+# - include user_stamp when authentication system is in place
+#
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    mapping/:id/comment endpoint
+
+    Serialize comment associated to mapping
+    """
+
+    # this is probably not needed, the framework should check
+    # the provided comment data is not blank
+    def validate_comment(self, value):
+        """
+        Check the comment is non-empty
+        """
+
+        if not value.translate({ord(" "):None, ord("\t"):None}):
+            raise serializer.ValidationError("Comment is empty")
+
+        return value
+
+    def create(self, validated_data):
+        return UeMappingComment.objects.create(**validated_data)
+
+    class Meta:
+        model = UeMappingComment
         exclude = ('user_stamp',)
