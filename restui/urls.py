90eac8cf05aa63b0d5e7322fe1599aa13cdd1da2
Matthew Laird <lairdm@gmail.com>
2018-07-20 08:25:43 +0100

    Update urls.py
    File renamed

--- restui/urls.py
+++ restui/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from django.conf.urls import url
-from restui.views import loaders, mappings
+from restui.views import ensembl, mappings
 
 urlpatterns = [
-    path('ensembl/load/<species>/<assembly_accession>/<int:ensembl_tax_id>/<int:ensembl_release>/', loaders.EnsemblFeature.as_view(), name='genome_detail'),
+    path('ensembl/load/<species>/<assembly_accession>/<int:ensembl_tax_id>/<int:ensembl_release>/', ensembl.EnsemblFeature.as_view(), name='genome_detail'),
     path('mapping/<int:pk>/status/', mappings.MappingStatusView.as_view()),
     path('mapping/<int:pk>/comments/', mappings.MappingCommentView.as_view()),
     path('mapping/<int:pk>/labels/<label>/', mappings.DeleteMappingLabelView.as_view()),
