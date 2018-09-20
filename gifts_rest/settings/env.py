bebc20861f979dfdce88d2eb5a43a8f60e02d0d4
Matthew Laird <lairdm@ebi.ac.uk>
2018-09-20 10:40:31 +0100

    Allow fallover mode where we don't allow writing on post/put endpoints

--- gifts_rest/settings/env.py
+++ gifts_rest/settings/env.py
@@ -1,6 +1,8 @@
 import os
 
 ENV      = os.getenv('DJANGO_ENVIRONMENT', 'development')
+FALLOVER = bool(os.getenv('FALLOVER', False))
+
 DEV_ENV  = ENV == 'development'
 TEST_ENV = ENV == 'staging'
 PROD_ENV = ENV == 'production'
