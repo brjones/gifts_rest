0268f8b37b3b433624abbafcb721d2d2a8c11855
Alessandro Vullo <avullo@ebi.ac.uk>
2019-03-22 11:28:57 +0000

    [EA-88]. Models to support schema update.

--- restui/models/annotations.py
+++ restui/models/annotations.py
@@ -27,6 +27,8 @@ class CvUeStatus(models.Model):
         managed = False
         db_table = 'cv_ue_status'
 
+# schema supporing editing comments/status for mapped entries
+
 class UeMappingComment(models.Model):
     id = models.BigAutoField(primary_key=True)
     time_stamp = models.DateTimeField()
@@ -62,3 +64,41 @@ class UeMappingStatus(models.Model):
     class Meta:
         managed = False
         db_table = 'ue_mapping_status'
+
+# and the same for unmapped entries
+
+class UeUnmappedEntryComment(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    time_stamp = models.DateTimeField()
+    user_stamp = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_column='user_stamp')
+    comment = models.TextField()
+    uniprot = models.ForeignKey('UniprotEntry', models.DO_NOTHING, related_name='comments', blank=True, null=True)
+    deleted = models.BooleanField()
+
+    class Meta:
+        managed = False
+        db_table = 'ue_unmapped_entry_comment'
+
+
+class UeUnmappedEntryLabel(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    time_stamp = models.DateTimeField()
+    user_stamp = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_column='user_stamp')
+    label = models.ForeignKey('CvUeLabel', models.DO_NOTHING, db_column='label')
+    uniprot = models.ForeignKey('UniprotEntry', models.DO_NOTHING, related_name='labels', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'ue_unmapped_entry_label'
+
+
+class UeUnMappedEntryStatus(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    time_stamp = models.DateTimeField()
+    user_stamp = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE, db_column='user_stamp')
+    status = models.ForeignKey('CvUeStatus', db_column='status', to_field='id', on_delete=models.CASCADE)
+    uniprot = models.ForeignKey('UniprotEntry', related_name='status_history', on_delete=models.CASCADE)
+
+    class Meta:
+        managed = False
+        db_table = 'ue_unmapped_entry_status'
