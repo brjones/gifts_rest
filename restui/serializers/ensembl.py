087e7d1856654315530a5fe46ab547e1074e11a5
Alessandro Vullo <avullo@ebi.ac.uk>
2019-04-29 16:48:49 +0100

    [EA-64]. Readapted to use the celery task.

--- restui/serializers/ensembl.py
+++ restui/serializers/ensembl.py
@@ -1,13 +1,9 @@
-import pprint
-from itertools import chain # flatten list of lists, i.e. list of transcripts for each gene
-
 from django.utils import timezone
 from django.core.serializers import serialize
 from rest_framework import serializers
-from psqlextra.query import ConflictAction
 
-from restui.models.ensembl import EnsemblGene, EnsemblTranscript, EnsemblSpeciesHistory, GeneHistory, TranscriptHistory, EnspUCigar
-
+from restui.tasks import bulk_upload_task
+from restui.models.ensembl import EnsemblTranscript, EnsemblSpeciesHistory, EnspUCigar
 
 class EnsemblTranscriptSerializer(serializers.Serializer):
     """
@@ -58,87 +54,12 @@ class EnsemblGeneListSerializer(serializers.ListSerializer):
     # Override using the bulk_insert behaviour from postgres-extra to allow fast insertion and return obj IDs so that
     # we can recursively insert gene transcripts
     def create(self, validated_data):
-        #
-        # create new species history, use required parameters passed to view from endpoint URL
-        #
-        # NOTE: filter is likely to be not necessary
-        #
+        # data necessary to create new load record, i.e. species history
         history_attrs = { k:v for (k,v) in self.context['view'].kwargs.items()
                           if k in ('species', 'assembly_accession', 'ensembl_tax_id', 'ensembl_release') }
-        history_attrs['status'] = 'LOAD_STARTED' # temporary status
-        history = EnsemblSpeciesHistory.objects.create(**history_attrs)
-
-        #
-        # transform incoming data for genes/transcripts in a way suitable for bulk insertion
-        #
-        # assume incoming data has list of transcripts nested into each gene
-        # map each ensg ID to the list of its transcripts, so that we can later
-        # assign the gene to each transcript for the transcripts bulk insert
-        gdata = []
-        tdata = {}
-        timestamp = timezone.now()
-
-        for item in validated_data:
-            item['time_loaded'] = timestamp # add timestamp to gene
-
-            ensg_id = item.get('ensg_id')
-
-            # need to remove 'transcripts' from data as this is not part of the gene model
-            #
-            # NOTE: assume payload always contains non-empty transcripts data for each gene
-            #
-            tdata[ensg_id] = item.pop('transcripts')
-
-            # add timestamp to gene's transcripts too
-            for t in tdata[ensg_id]:
-                t['time_loaded'] = timestamp
-
-            gdata.append(dict(**item))
-
-        #
-        # bulk insert the genes and transcripts
-        #
-        # WARNING
-        #
-        # From http://django-postgres-extra.readthedocs.io/manager/
-        # In order to stick to the "everything in one query" principle, various, more advanced usages of bulk_insert are impossible.
-        # It is not possible to have different rows specify different amounts of columns.
-        #
-        genes = EnsemblGene.objects.on_conflict(['ensg_id'], ConflictAction.UPDATE).bulk_insert(gdata, return_model=True)
-
-        # map each transcript data to its corresponding gene object,
-        # effectively establishing the gene-transcript one-to-many relationship
-        # use generator expression to reduce memory footprint
-        for t, g in ( (transcript_data, gene) for gene in genes for transcript_data in tdata[gene.ensg_id] ):
-            t["gene"] = g
         
-        # bulk insert the transcripts mapped to their genes
-        transcripts = EnsemblTranscript.objects.on_conflict(['enst_id'],
-                                                            ConflictAction.UPDATE).bulk_insert(list(chain.from_iterable(tdata.values())),
-                                                                                               return_model=True)
-        #
-        # insert genes and trascripts histories
-        #
-        # TODO: optimise, bulk_insert again?!
-        #
-        # UPDATE: tried bulk_insert, but receive a strange error:
-        #         'column "ensembl_species_history" does not exist'
-        #         HINT:  Perhaps you meant to reference the column "gene_history.ensembl_species_history_id"
-        #                or the column "excluded.ensembl_species_history_id".
-        #
-        # GeneHistory.objects.on_conflict(['ensembl_species_history', 'gene'],
-        #                                ConflictAction.UPDATE).bulk_insert([ dict(ensembl_species_history=history, gene=g) for g in genes ])
-        #
-        for gene in genes:
-            GeneHistory.objects.create(ensembl_species_history=history, gene=gene)
-        for transcript in transcripts:
-            TranscriptHistory.objects.create(ensembl_species_history=history, transcript=transcript)
+        return bulk_upload_task.delay(history = history_attrs, data = validated_data)
 
-        history.time_loaded = timezone.now() # WARNING: this generates datetime with microseconds and no UTC
-        history.status = 'LOAD_COMPLETE'
-        history.save()
-        
-        return genes
 
 class EnsemblGeneSerializer(serializers.Serializer):
     gene_id = serializers.IntegerField(required=False)
