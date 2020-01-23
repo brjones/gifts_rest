5137a7431a8c1cb7ba71959af20f41bd59685880
Sanjay Boddu <sboddu@ebi.ac.uk>
2020-01-23 14:07:10 +0000

    Dont need authentication for task status checking

--- restui/views/pipelines.py
+++ restui/views/pipelines.py
@@ -2,12 +2,8 @@ from celery.result import AsyncResult
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-
 
 class CheckJobStatus(APIView):
-
-    permission_classes = (IsAuthenticated,)
 
     def get(self, request, task_id):
         result = AsyncResult(task_id)
